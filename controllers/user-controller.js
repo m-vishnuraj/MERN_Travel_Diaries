5770db7a9727766951b046e84f530afa6218b268
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-20 19:19:14 +0530

    mvc get user completed

---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+import User from "../models/User";
+
+export const getAllUsers = async (req, res) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!users) {
+    return res.status(500).json({ message: "Unexpected Error Occurred" });
+  }
+  return res.status(200).json({ users });
+};
