2c8f1c2bd208abd0596eb83d5253e3b9ff9526f8
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-20 15:59:41 +0530

    mongo db connected successfully

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.use("/", (req, res, next) => {
-  return res.send("Hi");
-});
+// middleware
 
-app.listen(5000, () => console.log("Listening to localhost port 5000"));
+// connection to database
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.hpilm34.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connection successful & Listening to localhost port 5000")
+    )
+  )
+  .catch((err) => console.log(err));
