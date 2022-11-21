2c474934137769130e7f9d4ffac504733b54e354
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-21 09:24:19 +0530

    mvc sign up user completed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,4 @@
+import { hashSync } from "bcryptjs";
 import User from "../models/User";
 
 export const getAllUsers = async (req, res) => {
@@ -11,4 +12,34 @@ export const getAllUsers = async (req, res) => {
     return res.status(500).json({ message: "Unexpected Error Occurred" });
   }
   return res.status(200).json({ users });
+};
+
+// `````````````````````````
+export const signUp = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.length < 6
+  ) {
+    return res.status(422).json({ message: "Invalid Inputs" });
+  }
+
+  const hashedPassword = hashSync(password);
+  let user;
+  try {
+    user = new User({ email, name, password: hashedPassword });
+    await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!user) {
+    return res.status(500).json({ message: "Unexpected Error Occurred" });
+  }
+
+  return res.status(201).json({ message: "User Created", user });
 };
--- routing/user-routes.js
+++ routing/user-routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controller";
+import { getAllUsers, signUp } from "../controllers/user-controller";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
+userRouter.post("/signup", signUp);
 
 export default userRouter;
