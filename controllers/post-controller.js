829ba7472e2c90e300e24723718f2e38503571d7
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-22 05:58:11 +0530

    get post by id completed

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,5 +1,6 @@
 import Post from "../models/Post";
 
+// get all posts
 export const getAllPost = async (req, res) => {
   let posts;
   try {
@@ -14,6 +15,7 @@ export const getAllPost = async (req, res) => {
   res.status(200).json({ posts: posts });
 };
 
+// add post
 export const addPost = async (req, res) => {
   const { title, description, image, location, date, user } = req.body;
   if (
@@ -50,3 +52,26 @@ export const addPost = async (req, res) => {
   }
   return res.status(201).json({ post: post });
 };
+
+// get post by id
+
+export const getPostById = async (req, res) => {
+  const id = req.params.id;
+
+  let post;
+
+  try {
+    post = await Post.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+
+  return res.status(200).json({ post: post });
+};
+
+// update post
+
+// delete post
--- routing/post-routes.js
+++ routing/post-routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { addPost, getAllPost } from "../controllers/post-controller";
+import {
+  addPost,
+  getAllPost,
+  getPostById,
+} from "../controllers/post-controller";
 
 const postRouter = Router();
 
 postRouter.get("/", getAllPost);
+postRouter.get("/:id", getPostById);
 postRouter.post("/", addPost);
 
 export default postRouter;
