4a8dd2fcf8c55a2c07cddebe604d9d09b02402d3
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-23 11:28:06 +0530

    Diaries api configured all posts

--- frontend/src/diaries/Diaries.jsx
+++ frontend/src/diaries/Diaries.jsx
@@ -1,25 +1,39 @@
 import { Box } from '@mui/material';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAllPosts } from '../api-helpers/Helpers';
 import DiaryItem from './DiaryItem';
 
 const Diaries = () => {
+    const [posts, setPosts] = useState();
     useEffect(() => {
-        getAllPosts().then((data) => console.log(data))
-            .catch(err => console.log(err));
-    }, [])
+        getAllPosts()
+            .then((data) => setPosts(data?.posts))
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
-        <Box display="flex"
+        <Box
+            display="flex"
             flexDirection={"column"}
             padding={3}
-            justifyContent={"center"}
-            alignItems={"center"}>
-            {""}
-            {[1, 2, 3, 4, 5].map((item) => {
-                return <DiaryItem key={item} />
-            })}
-            <DiaryItem />
+            justifyContent="center"
+            alignItems={"center"}
+        >
+            {" "}
+            {posts &&
+                posts.map((item, index) => (
+                    <DiaryItem
+                        date={new Date(`${item.date}`).toLocaleDateString()}
+                        description={item.description}
+                        image={item.image}
+                        id={item._id}
+                        location={item.location}
+                        title={item.title}
+                        key={index}
+                        user={item.user._id}
+                        name={item.user.name}
+                    />
+                ))}
         </Box>
     )
 }
--- frontend/src/diaries/DiaryItem.jsx
+++ frontend/src/diaries/DiaryItem.jsx
@@ -5,7 +5,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { Box } from '@mui/system';
 
-const DiaryItem = () => {
+const DiaryItem = ({ title, description, image, location, date, id }) => {
     return (
         <Card sx={{
             width: "50%",
@@ -27,18 +27,18 @@ const DiaryItem = () => {
                         {<EditLocationAltIcon />}
                     </IconButton>
                 }
-                title="Shrimp and Chorizo Paella"
-                subheader="September 14, 2016"
+                title={location}
+                subheader={date}
             />
             <CardMedia
                 component="img"
                 height="194"
-                src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                alt="Paella dish"
+                src={image}
+                alt={title}
             />
             <CardContent>
                 <Typography paddingBottom={1} variant="h6" color="text.secondary">
-                    This impressive paella is a perfect party dish.
+                    {title}
                 </Typography>
                 <hr />
                 <Box paddingTop={1} display={"flex"} >
@@ -46,9 +46,7 @@ const DiaryItem = () => {
                         Vishnuraj M
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
+                        {description}
                     </Typography>
                 </Box>
             </CardContent>
