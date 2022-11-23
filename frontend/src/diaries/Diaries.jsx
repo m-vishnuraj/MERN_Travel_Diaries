2ee57dc81e8695b929a8c1039dd86826fbf49eb8
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-23 08:30:30 +0530

    card design completed

--- frontend/src/diaries/Diaries.jsx
+++ frontend/src/diaries/Diaries.jsx
@@ -1,8 +1,20 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import DiaryItem from './DiaryItem'
 
 const Diaries = () => {
     return (
-        <div>Diaries</div>
+        <Box display="flex"
+            flexDirection={"column"}
+            padding={3}
+            justifyContent={"center"}
+            alignItems={"center"}>
+            {""}
+            {[1, 2, 3, 4, 5].map((item) => {
+                return <DiaryItem key={item} />
+            })}
+            <DiaryItem />
+        </Box>
     )
 }
 
