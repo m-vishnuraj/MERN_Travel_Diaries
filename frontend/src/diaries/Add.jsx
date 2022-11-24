1b8df451fc46dd7920ae72955420b546ee89aac2
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-24 16:25:45 +0530

    Handling Add Form

--- frontend/src/diaries/Add.jsx
+++ frontend/src/diaries/Add.jsx
@@ -1,51 +1,103 @@
 import { Box, Button, FormLabel, TextField, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 
 const Add = () => {
+    const [inputs, setInputs] = useState({
+        title: '',
+        description: '',
+        date: '',
+        location: '',
+        image: '',
+    });
+    const handleChange = (e) => {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputs);
+    };
+
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            width="100%"
-            height="100%">
+        <Box display="flex" flexDirection={"column"} width="100%" height="100%">
             <Box display="flex" margin="auto" padding={2}>
                 <Typography
+                    fontWeight={"bold"}
                     variant="h4"
-                    fontWeight="bold"
-                    fontFamily={"dancing script"}>
-                    Add Your Diaries
+                    fontFamily={"dancing script"}
+                >
+                    Add Your Travel Diary
                 </Typography>
                 <TravelExploreIcon
-                    sx={{ fontSize: "40px", paddingLeft: 1, color: "lightcoral" }} />
+                    sx={{ fontSize: "40px", paddingLeft: 1, color: "lightcoral  " }}
+                />
             </Box>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Box
                     padding={3}
                     display="flex"
                     width="80%"
                     margin="auto"
-                    flexDirection={"column"}>
+                    flexDirection={"column"}
+                >
                     <FormLabel sx={{ fontFamily: "quicksand" }}>Title</FormLabel>
-                    <TextField variant='standard' margin='normal' />
+                    <TextField
+                        onChange={handleChange}
+                        name="title"
+                        value={inputs.title}
+                        variant="standard"
+                        margin="normal"
+                    />
                     <FormLabel sx={{ fontFamily: "quicksand" }}>Description</FormLabel>
-                    <TextField variant='standard' margin='normal' />
+                    <TextField
+                        onChange={handleChange}
+                        name="description"
+                        value={inputs.description}
+                        variant="standard"
+                        margin="normal"
+                    />
                     <FormLabel sx={{ fontFamily: "quicksand" }}>Image URL</FormLabel>
-                    <TextField variant='standard' margin='normal' />
+                    <TextField
+                        onChange={handleChange}
+                        name="imageUrl"
+                        value={inputs.imageUrl}
+                        variant="standard"
+                        margin="normal"
+                    />
+
                     <FormLabel sx={{ fontFamily: "quicksand" }}>Location</FormLabel>
-                    <TextField variant='standard' margin='normal' />
+                    <TextField
+                        onChange={handleChange}
+                        name="location"
+                        value={inputs.location}
+                        variant="standard"
+                        margin="normal"
+                    />
                     <FormLabel sx={{ fontFamily: "quicksand" }}>Date</FormLabel>
-                    <TextField variant='standard' margin='normal' />
+                    <TextField
+                        type="date"
+                        onChange={handleChange}
+                        name="date"
+                        value={inputs.date}
+                        variant="standard"
+                        margin="normal"
+                    />
                     <Button
-                        sx={{ width: "50% ", margin: "auto", mt: 2, borderRadius: "10px" }}
-                        color='warning'
-                        variant='contained'>
+                        type="submit"
+                        color="warning"
+                        sx={{ width: "50%", margin: "auto", mt: 2, borderRadius: 10 }}
+                        variant="contained"
+                    >
                         Post
                     </Button>
                 </Box>
             </form>
         </Box>
-    )
-}
+    );
+};
 
 export default Add
