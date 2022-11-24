8938ac2e2863084a0c3bea2fe603450c39217023
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-24 11:22:55 +0530

    Adding Different Header Links After Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Auth from "./auth/Auth";
+import Add from "./diaries/Add";
 import Diaries from "./diaries/Diaries";
 import Header from "./header/Header";
 import Home from "./home/Home";
+import Profile from "./profile/Profile";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
@@ -18,6 +20,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/diaries" element={<Diaries />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </section>
     </div>
