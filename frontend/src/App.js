e7f4caa391c1e120e75dae7b4a68ac7ae5ef1476
m-vishnuraj <mvishnurajm@gmail.com>
2022-11-26 05:05:20 +0530

    delete post completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./auth/Auth";
 import Add from "./diaries/Add";
 import Diaries from "./diaries/Diaries";
+import DiaryUpdate from "./diaries/DiaryUpdate";
 import Header from "./header/Header";
 import Home from "./home/Home";
 import Profile from "./profile/Profile";
@@ -22,10 +23,9 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/add" element={<Add />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/post/:id" element={<DiaryUpdate />} />
         </Routes>
       </section>
     </div>
   );
 }
-
-export default App;
